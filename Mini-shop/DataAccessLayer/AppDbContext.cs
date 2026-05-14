0b0a072f44c71a50f0b659921904c157649a0465
vusale44 <vusalesalman1802@gmail.com>
2026-05-14 09:18:27 +0400

    bezdimm gava oyrenecem

---
+++ Mini-shop/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_shop.Models;
+
+namespace Mini_shop.DataAccessLayer
+{
+   
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Card> Card { get; set; }
+        
+    }
+    }
+
---
+++ Mini-shop/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Mini_shop.DataAccessLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        x =>
+        {
+            x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
